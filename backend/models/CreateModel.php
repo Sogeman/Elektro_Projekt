062a350204a20c26602e52eb828046201ba88b31
Rene Mandler <pw17l006@technikum-wien.at>
2018-07-11 10:06:25 +0200

    Datenbank bis auf FI/Sicherungen vollständig, Create Methoden fertig

--- backend/models/CreateModel.php
+++ backend/models/CreateModel.php
@@ -1,30 +1,101 @@
 <?php
 
-/* 
+/*
  * Rene
  */
+
 // Nicht sicher ob das so funktioniert, muss ich noch testen
 class CreateModel {
-    
+
     private $database;
-     
+
     public function __construct() {
         $this->database = new Database(DBHost, DBName, DBUser, DBPass);
     }
-    
+
     public function createProject($specification) {
         $name = $specification->name;
+
         try {
             $sql = "INSERT into projects (name) VALUES (:name)";
             $stmt = $this->database->prepare($sql);
             $stmt->bindParam(':name', $name);
             $stmt->execute();
-            return true;
-        }
-        catch(PDOException $e)
-        {
+            return $this->database->lastInsertedId(); 
+        } catch (PDOException $e) {
+            echo $e->getTraceAsString();
             return false;
         }
-        
     }
+
+    public function createFloor($specification, $parentid) {
+        $name = $specification->name;
+        $count = $specification->floor_count_from_basement;
+
+        try {
+            $sql = "INSERT into floors (projects_id, floor_count_from_basement, name) VALUES (:id, :count, :name)";
+            $stmt = $this->database->prepare($sql);
+            $stmt->bindParam(":id", $parentid);
+            $stmt->bindParam(":count", $count);
+            $stmt->bindParam(":name", $name);
+            $stmt->execute();
+            return $this->database->lastInsertedId(); 
+        } catch (PDOException $e) {
+            echo $e->getTraceAsString();
+            return false;
+        }
+    }
+    
+    public function createRoom($specification, $parentid) {
+        $name = $specification->name;
+
+        try {
+            $sql = "INSERT into rooms (floors_id, name) VALUES (:id, :name)";
+            $stmt = $this->database->prepare($sql);
+            $stmt->bindParam(":id", $parentid);
+            $stmt->bindParam(":name", $name);
+            $stmt->execute();
+            return $this->database->lastInsertedId(); 
+        } catch (PDOException $e) {
+            echo $e->getTraceAsString();
+            return false;
+        }
+    }
+    
+    public function createDevice($specification, $parentid) {
+        $name = $specification->name;
+
+        try {
+            $sql = "INSERT into devices (rooms_id, name) VALUES (:id, :name)";
+            $stmt = $this->database->prepare($sql);
+            $stmt->bindParam(":id", $parentid);
+            $stmt->bindParam(":name", $name);
+            $stmt->execute();
+            return $this->database->lastInsertedId(); 
+        } catch (PDOException $e) {
+            echo $e->getTraceAsString();
+            return false;
+        }
+    }
+    
+    public function createSensor($specification, $parentid) {
+        $name = $specification->name;
+        $unit = $specification->unit;
+        $value = $specification->value;
+
+        try {
+            $sql = "INSERT into sensors (devices_id, name, unit, value) VALUES (:id, :name, :unit, :value)";
+            $stmt = $this->database->prepare($sql);
+            $stmt->bindParam(":id", $parentid);
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":unit", $unit);
+            $stmt->bindParam(":value", $value);
+            $stmt->execute();
+            return $this->database->lastInsertedId(); 
+        } catch (PDOException $e) {
+            echo $e->getTraceAsString();
+            return false;
+        }
+    }
+
 }
--- backend/services/Database.php
+++ backend/services/Database.php
@@ -5,39 +5,44 @@
  *
  * @author helmuth
  */
-class Database{
+class Database {
+
     private $pdo;
     public $lastInsertedId;
-    
-    
-    public function __construct($dbHost, $dbName, $dbUser,$dpPass){
-        $this->pdo = new PDO("mysql:host=".$dbHost.";dbname=".$dbName.";charset=utf8", $dbUser, $dpPass);
+
+    public function __construct($dbHost, $dbName, $dbUser, $dpPass) {
+        $this->pdo = new PDO("mysql:host=" . $dbHost . ";dbname=" . $dbName . ";charset=utf8", $dbUser, $dpPass);
     }
 
-    public function query($sql){
+    public function query($sql) {
         $resultTable = array();
-        
-        try{
+
+        try {
             foreach ($this->pdo->query($sql) as $row) {
                 $resultTable[] = $row;
             }
-        } catch (PDOException $ex){
-            error_log("PDO ERROR: querying database: " . $ex->getMessage()."\n".$sql);
+        } catch (PDOException $ex) {
+            error_log("PDO ERROR: querying database: " . $ex->getMessage() . "\n" . $sql);
             return $resultTable;
         }
-        
+
         return $resultTable;
     }
-    
-    public function order($sql){
-        try{
+
+    public function order($sql) {
+        try {
             $this->pdo->query($sql);
         } catch (PDOException $ex) {
-            error_log("PDO ERROR: querying database: " . $ex->getMessage()."\n".$sql);
+            error_log("PDO ERROR: querying database: " . $ex->getMessage() . "\n" . $sql);
         }
     }
     
-    public function lastInsertedId(){
+    public function prepare($sql) {
+        return $this->pdo->prepare($sql);
+    }  
+
+    public function lastInsertedId() {
         return $this->pdo->lastInsertId('id');
     }
+
 }
