2c713606f75340727db9ef619dda5bfd358d5d4b
Rene Mandler <pw17l006@technikum-wien.at>
2018-07-11 10:07:21 +0200

    Datenbank update

--- projekt_2sem.sql
+++ projekt_2sem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 06. Jul 2018 um 20:13
+-- Erstellungszeit: 11. Jul 2018 um 10:06
 -- Server-Version: 10.1.31-MariaDB
 -- PHP-Version: 7.2.3
 
@@ -25,6 +25,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `devices`
+--
+
+CREATE TABLE `devices` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `rooms_id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `devices`
+--
+
+INSERT INTO `devices` (`id`, `rooms_id`, `name`, `created`) VALUES
+(1, 1, 'Testlicht', '2018-07-11 07:59:19');
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `floors`
 --
 
@@ -35,6 +55,13 @@ CREATE TABLE `floors` (
   `name` varchar(255) CHARACTER SET utf8 NOT NULL,
   `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `floors`
+--
+
+INSERT INTO `floors` (`id`, `projects_id`, `floor_count_from_basement`, `name`, `created`) VALUES
+(4, 6, 1, 'Test-Erdgeschoss', '2018-07-11 07:50:36');
 
 -- --------------------------------------------------------
 
@@ -55,11 +82,60 @@ CREATE TABLE `projects` (
 INSERT INTO `projects` (`id`, `name`, `created`) VALUES
 (1, 'Testhaus', '2018-07-03 11:13:13'),
 (2, 'Testhaus 2', '2018-07-06 16:24:21'),
-(3, 'Testhaus 3', '2018-07-06 16:24:21');
+(3, 'Testhaus 3', '2018-07-06 16:24:21'),
+(6, 'Test-Projekt', '2018-07-11 07:33:39');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `floors_id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `floors_id`, `name`, `created`) VALUES
+(1, 4, 'Testraum', '2018-07-11 07:55:56');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `sensors`
+--
+
+CREATE TABLE `sensors` (
+  `id` int(11) NOT NULL,
+  `devices_id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `unit` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `value` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `sensors`
+--
+
+INSERT INTO `sensors` (`id`, `devices_id`, `name`, `unit`, `value`, `created`) VALUES
+(1, 1, 'Testschalter', 'ein/aus', 'aus', '2018-07-11 08:05:04');
 
 --
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `devices`
+--
+ALTER TABLE `devices`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indizes für die Tabelle `floors`
@@ -74,20 +150,50 @@ ALTER TABLE `projects`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indizes für die Tabelle `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `sensors`
+--
+ALTER TABLE `sensors`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT für exportierte Tabellen
 --
+
+--
+-- AUTO_INCREMENT für Tabelle `devices`
+--
+ALTER TABLE `devices`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT für Tabelle `floors`
 --
 ALTER TABLE `floors`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT für Tabelle `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT für Tabelle `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT für Tabelle `sensors`
+--
+ALTER TABLE `sensors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
