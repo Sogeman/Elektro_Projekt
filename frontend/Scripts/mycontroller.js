497331814f3ac9a7cda03442f8f1cead2a7af659
Rene Mandler <pw17l006@technikum-wien.at>
2018-08-05 20:29:52 +0200

    no message

--- frontend/Scripts/mycontroller.js
+++ frontend/Scripts/mycontroller.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
 // 	"action": "create",
 // 	"listtype": "FLOORS",
 // 	"parentid": 1,
-// 	"itemid": 1,
 // 	"specification": {
 // 		"name": "Erdgeschoss",
 // 		"floor_count_from_basement": 1
