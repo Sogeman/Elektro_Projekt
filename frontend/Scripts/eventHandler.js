f5cb7238aef6f399e197a81c2ec63d27f81b6efb
Rene Mandler <pw17l006@technikum-wien.at>
2018-08-05 20:25:44 +0200

    support for cb and fuse buttons and in modal for entry creation

--- frontend/Scripts/eventHandler.js
+++ frontend/Scripts/eventHandler.js
@@ -1,16 +1,16 @@
 var EventHandler = {
 
-    Initialize: function() {
+    Initialize: function () {
         QueryManager.LoadingScreen();
         QueryManager.LoadData(Controller.homepage);
         EventHandler.HomeEvent();
         EventHandler.CreateEvent();
         EventHandler.BackButton();
     }
-    
-    , TableEvents: function(serverData) {
+
+    , TableEvents: function (serverData) {
         // Event for going to the next level
-        $(".clickable").off().on("click", function(event) {
+        $(".clickable").off().on("click", function (event) {
             event.stopPropagation();
 
             var request;
@@ -20,99 +20,125 @@ var EventHandler = {
             $("#parent-id").attr("value", parentId);
             var name = clickedItem[0].name;
 
-            request = {action: "list", listtype: nextLevel, parentid: parentId};
-            QueryManager.LoadData(request);
-
             if (QueryManager.currentLevel == "projects") {
                 $("#page-title").text(name);
+                QueryManager.projectId = parentId;
             }
-            
+            if (QueryManager.currentLevel != "circuitbreakers" && QueryManager.currentLevel != "fuses" && QueryManager.currentLevel != "sensors") {
+                request = { action: "list", listtype: nextLevel, parentid: parentId };
+                QueryManager.LoadData(request);
+            }
         });
 
         // Events for delete, edit and shopping list buttons
-        $(".edit-button").off().on("click", function(event) {
+        $(".edit-button").off().on("click", function (event) {
             event.stopPropagation();
             ModalManager.EditEntry($(this), serverData);
         });
-        $(".delete-button").off().on("click", function(event) { 
+        $(".delete-button").off().on("click", function (event) {
             event.stopPropagation();
             ModalManager.DeleteWarning($(this), serverData);
-            
+
         });
-        $(".shopping-list-button").off().on("click", function(event) {
+        $(".shopping-list-button").off().on("click", function (event) {
             event.stopPropagation();
             var clickedItem = EventHandler.FindClickedItem($(this), serverData);
             var id = clickedItem[0].id;
             var name = clickedItem[0].name;
-            var item = {id: id, name: name};
+            var item = { id: id, name: name };
             ShoppingListHandler.RequestShoppingList(item);
-            
+
         });
     }
 
-    , ConfirmDeletion: function(request) {
-        $("#delete-warning-confirm").off().on("click", function(event) {
+    , cbAndFuseButtons: function () {
+        $("#circuitbreaker-button").off().on("click", function (event) {
+            event.stopPropagation();
+            var request = { action: "list", listtype: "circuitbreakers", parentid: QueryManager.projectId };
+            QueryManager.LoadData(request);
+        });
+
+        $("#fuse-button").off().on("click", function (event) {
+            event.stopPropagation();
+            var request = { action: "list", listtype: "fuses", parentid: QueryManager.projectId };
+            QueryManager.LoadData(request);
+        })
+    }
+
+    , ConfirmDeletion: function (request) {
+        $("#delete-warning-confirm").off().on("click", function (event) {
             event.stopPropagation();
             $("#delete-warning-modal").modal("hide");
             $("#delete-confirm-modal").modal();
-            $("#delete-confirm").off().on("click", function(event) {
+            $("#delete-confirm").off().on("click", function (event) {
                 event.stopPropagation();
                 ModalManager.DeleteEntry(request);
                 $("#delete-confirm-modal").modal("hide");
-                
+
             });
-        
+
         });
     }
 
-    , HomeEvent: function() {
+    , HomeEvent: function () {
         $("#home").on("click", function (event) {
             event.stopPropagation();
+            QueryManager.lastRequests.splice(0, QueryManager.lastRequests.length);
             QueryManager.LoadData(Controller.homepage);
-            $("#page-title").text("Projekte");  
+            $("#page-title").text("Projekte");
         });
     }
 
-    , SaveEvent: function(data, action) {
-        $("#save-button").off().on("click", function(event) {
+    , SaveEvent: function (action) {
+        $("#save-button").off().on("click", function (event) {
             event.stopPropagation();
-            ModalManager.SaveEntry(data, action);
-            
+            ModalManager.SaveEntry(action);
         });
     }
 
-    , CreateEvent: function() {
-        $("#create-item-button").off().on("click", function(event) {
+    , CreateEvent: function () {
+        $("#create-item-button").off().on("click", function (event) {
             event.stopPropagation();
+            ModalManager.ClearModal();
             $("#create-entry-modal").modal(); //enables modal manually
             switch (QueryManager.currentLevel) {
                 case "devices":
-                ModalManager.devicesSelect.change(function() {
-                    var value = ModalManager.devicesSelect.val();
-                    ModalManager.formName.find("input").val(value);
-                });
+                    ModalManager.devicesSelect.change(function () {
+                        var value = ModalManager.devicesSelect.val();
+                        ModalManager.formName.find("input").val(value);
+                    });
                     break;
                 case "sensors":
-                ModalManager.sensorsSelect.change(function() {
-                    var value = ModalManager.sensorsSelect.val();
-                    ModalManager.formName.find("input").val(value);
-                });
+                    ModalManager.sensorsSelect.change(function () {
+                        var value = ModalManager.sensorsSelect.val();
+                        ModalManager.formName.find("input").val(value);
+                    });
+                    break;
+                case "circuitbreakers":
+                    var request = { action: "list", listtype: "floors", parentid: QueryManager.projectId };
+                    QueryManager.LoadDataSimple(request);
+                    break;
+                case "fuses":
+                    var request = { action: "list", listtype: "rooms", parentid: QueryManager.projectId };
+                    QueryManager.LoadDataSimple(request);
+                    request = { action: "list", listtype: "circuitbreakers", parentid: QueryManager.projectId };
+                    QueryManager.LoadDataSimple(request);
                     break;
                 default:
                     break;
             }
-            ModalManager.CreateEntry();  
+            ModalManager.CreateEntry();
         });
     }
 
-    , ClearModal: function() {
+    , ClearModal: function () { // clears Modal when it's closed
         $("#create-entry-modal").off().on("hidden.bs.modal", function (event) {
             ModalManager.ClearModal();
         });
     }
 
-    , BackButton: function() {
-        $("#back-button").off().on("click", function(event) {
+    , BackButton: function () {
+        $("#back-button").off().on("click", function (event) {
             event.stopPropagation();
             QueryManager.GoBackToPreviousLevel();
         });
@@ -132,11 +158,4 @@ var EventHandler = {
         var clickedItem = allItemsArray.filter((obj) => obj);
         return clickedItem;
     }
-
-    // , CloseEvent: function() {
-    //     $(".close-button").off().on("click", function(event) {
-    //         event.stopPropagation();
-    //         $('[data-toggle="tooltip"]').tooltip({trigger: "hover"});
-    //     });
-    // }
 }
