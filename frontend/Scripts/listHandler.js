09f1fba381be3a6ad706b3990854bb99e60728b0
Rene Mandler <32226747+Sogeman@users.noreply.github.com>
2018-08-09 10:58:31 +0200

    Update listHandler.js

--- frontend/Scripts/listHandler.js
+++ frontend/Scripts/listHandler.js
@@ -70,7 +70,7 @@ var ListHandler = {
     } 
 
     , ListIsEmpty: function () {
-         var row = '<td colspan = 3 class="text-center"><h4>Nothing here, why not add something.</h4></td>';
+         var row = '<td colspan = 3 class="text-center"><h4>Nichts hier, bitte etwas hinzufügen</h4></td>';
         if(QueryManager.currentLevel != "projects") {
             row += '<td><button class="btn btn-link back-button" title="zurück"><img src="Content/images/back-arrow.png"></td>';
         }
