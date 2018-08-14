48833b9266246c809e6d8275e7ecc9163e34d02e
Rene Mandler <pw17l006@technikum-wien.at>
2018-08-14 13:52:05 +0200

    added message for empty modal list, changed fuse table look

--- frontend/Scripts/listHandler.js
+++ frontend/Scripts/listHandler.js
@@ -1,21 +1,13 @@
 var ListHandler = {
 
     FillTable: function (serverData) {
-        if (serverData.listtype != "fuses") {
-            $("#fuses-table-head").hide();
-            $("#normal-table-head").show();
-            ListHandler.FillNormalTable(serverData);
-        } else {
-            $("#normal-table-head").hide();
-            $("#fuses-table-head").show();
-            ListHandler.FillFusesTable(serverData);
-        }
+        ListHandler.FillDataIntoTable(serverData);
         EventHandler.TableEvents(serverData);
     }
 
-    , FillNormalTable: function (serverData) {
+    , FillDataIntoTable: function (serverData) {
         $("#data-table").empty();
-        console.log(serverData);
+        //console.log(serverData);
         var list = serverData["items"];
         var listtype = serverData["listtype"];
 
@@ -35,51 +27,32 @@ var ListHandler = {
             });
         }
     }
-    
-    , FillFusesTable: function (serverData) {
-        $("#data-table").empty();
-        var list = serverData["items"];
-        var listtype = serverData["listtype"];
-        if (list.length == 0) {
-            ListHandler.ListIsEmpty();
-        } else {
-            list.forEach(element => {
-                var id = element.id;
-                var name = element.name;
-                var circuitbreaker = element.circuitbreakername;
-                var row = '<tr class="single-entry" listtype="' + listtype + '" itemid="' + id + '">';
-                row += '<th scope="row" class="clickable">' + id + '</th>'
-                row += '<td class="clickable">' + name + '</td>';
-                row += '<td class="clickable">' + circuitbreaker + '</td>';
-                row += ListHandler.AddOptionButtons(listtype);
-                $("#data-table").append(row);
-            });
-        }
-    }
 
-    , AddOptionButtons: function(listtype) {
+    , AddOptionButtons: function (listtype) {
         var row = '<td class="py-0 pl-0 text-center"><button class="btn btn-link edit-button" title="Eintrag bearbeiten"><img src="frontend/Content/images/edit-icon.png"></button>';
         row += '<button class="btn btn-link delete-button" title="Eintrag löschen"><img src="frontend/Content/images/delete-icon.png">';
         if (listtype == "projects") {
             row += '<button class="btn btn-link shopping-list-button" title="Einkaufsliste anzeigen"><img src="frontend/Content/images/shopping-cart.png">';
-            row += '<button class="btn btn-link schematic-button" title="Schaltplan anzeigen"><img src="frontend/Content/images/schematic-button.png">';            
+            row += '<button class="btn btn-link schematic-button" title="Schaltplan anzeigen"><img src="frontend/Content/images/schematic-button.png">';
         } else {
             row += '<button class="btn btn-link back-button" title="zurück"><img src="frontend/Content/images/back-arrow.png">';
         }
         row += '</td></tr>';
         return row;
-    } 
+    }
 
     , ListIsEmpty: function () {
-         var row = '<td colspan = 3 class="text-center"><h4 class="mt-2">Nichts hier, bitte etwas hinzufügen</h4></td>';
-        if(QueryManager.currentLevel != "projects") {
+        var row = '<td colspan = 3 class="text-center"><h4 class="mt-2">Nichts hier, bitte etwas hinzufügen</h4></td>';
+        if (QueryManager.currentLevel != "projects") {
             row += '<td><button class="btn btn-link back-button" title="zurück"><img src="frontend/Content/images/back-arrow.png"></td>';
         }
         $("#data-table").append(row);
     }
 
     , CreateSelectOption: function (serverData) {
+        console.log(serverData);
         var level = serverData["listtype"];
+        var items = serverData["items"];
         $('#' + level + '-select').siblings().remove();
         var list = serverData["items"];
         var row = "";
@@ -87,6 +60,9 @@ var ListHandler = {
             row += '<option value="' + element.id + '">' + element.name + '</option>';
         });
         $('#' + level + '-select').after(row);
+        if (items.length == 0) {
+            $("#current-choice").attr("placeholder", "Keine Sicherung angelegt");
+        }
     }
 
 };
